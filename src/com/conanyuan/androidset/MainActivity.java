4b3b6495aa998d04b65ecbca86e83e63ee8e4175
Conan Yuan <conanyuan@gmail.com>
2013-02-27 02:42:46 -0500

    fix parceling of Deck

--- src/com/conanyuan/androidset/MainActivity.java
+++ src/com/conanyuan/androidset/MainActivity.java
@@ -111,12 +111,12 @@ public class MainActivity extends FragmentActivity {
      */
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
         outState.putIntegerArrayList("mFound", mFound);
         outState.putIntegerArrayList("mSets", mSets);
         outState.putIntegerArrayList("mShownCards", mShownCards);
         outState.putBooleanArray("mSelected", mSelected);
         outState.putParcelable("mDeck", mDeck);
+        super.onSaveInstanceState(outState);
     }
 
     @Override
@@ -260,8 +260,7 @@ public class MainActivity extends FragmentActivity {
             Toast.makeText(this, "End of Deck!", Toast.LENGTH_LONG).show();
             return;
         }
-        while (!mDeck.endOfDeck()
-                && (!setExists() || mShownCards.size() < N_INIT_CARDS))
+        while (!mDeck.endOfDeck() && (!setExists() || mShownCards.size() < N_INIT_CARDS))
         {
             for (int ii = 0; ii < N_AT_A_TIME; ii++) {
                 mShownCards.add(mDeck.nextCard());
@@ -292,9 +291,10 @@ public class MainActivity extends FragmentActivity {
         }
 
         if (isSet(selected)) {
-            Toast.makeText(this,
-                    "Found a set!  (Out of " + (mSets.size()/3) + " sets in view)",
-                    Toast.LENGTH_SHORT).show();
+            Toast.makeText(
+                    this,
+                    "Found a set!  (Out of " + (mSets.size() / 3)
+                            + " sets in view)", Toast.LENGTH_SHORT).show();
             for (int pos : selpos) {
                 mFound.add(mShownCards.get(pos));
             }
@@ -382,7 +382,7 @@ public class MainActivity extends FragmentActivity {
         private Deck(Parcel in) {
             mRand = new Random();
             mCurrent = in.readInt();
-            in.readIntArray(mCards);
+            mCards = in.createIntArray();
         }
 
         @Override
@@ -391,18 +391,21 @@ public class MainActivity extends FragmentActivity {
             out.writeIntArray(mCards);
         }
 
+        //@formatter:off
         public static final Parcelable.Creator<Deck> CREATOR =
-            new Parcelable.Creator<Deck>() {
-                public Deck createFromParcel(Parcel in)
-                {
-                    return new Deck(in);
-                }
+            new Parcelable.Creator<Deck>()
+            {
+            public Deck createFromParcel(Parcel in)
+            {
+                return new Deck(in);
+            }
 
-                public Deck[] newArray(int size)
-                {
-                    return new Deck[size];
-                }
+            public Deck[] newArray(int size)
+            {
+                return new Deck[size];
+            }
         };
+        // @formatter:on
     }
 
     /**
@@ -707,31 +710,84 @@ public class MainActivity extends FragmentActivity {
      * zero indexed variables.
      */
     private static Integer[] mImageIds = { R.drawable.card_1,
-            R.drawable.card_2, R.drawable.card_3, R.drawable.card_4,
-            R.drawable.card_5, R.drawable.card_6, R.drawable.card_7,
-            R.drawable.card_8, R.drawable.card_9, R.drawable.card_10,
-            R.drawable.card_11, R.drawable.card_12, R.drawable.card_13,
-            R.drawable.card_14, R.drawable.card_15, R.drawable.card_16,
-            R.drawable.card_17, R.drawable.card_18, R.drawable.card_19,
-            R.drawable.card_20, R.drawable.card_21, R.drawable.card_22,
-            R.drawable.card_23, R.drawable.card_24, R.drawable.card_25,
-            R.drawable.card_26, R.drawable.card_27, R.drawable.card_28,
-            R.drawable.card_29, R.drawable.card_30, R.drawable.card_31,
-            R.drawable.card_32, R.drawable.card_33, R.drawable.card_34,
-            R.drawable.card_35, R.drawable.card_36, R.drawable.card_37,
-            R.drawable.card_38, R.drawable.card_39, R.drawable.card_40,
-            R.drawable.card_41, R.drawable.card_42, R.drawable.card_43,
-            R.drawable.card_44, R.drawable.card_45, R.drawable.card_46,
-            R.drawable.card_47, R.drawable.card_48, R.drawable.card_49,
-            R.drawable.card_50, R.drawable.card_51, R.drawable.card_52,
-            R.drawable.card_53, R.drawable.card_54, R.drawable.card_55,
-            R.drawable.card_56, R.drawable.card_57, R.drawable.card_58,
-            R.drawable.card_59, R.drawable.card_60, R.drawable.card_61,
-            R.drawable.card_62, R.drawable.card_63, R.drawable.card_64,
-            R.drawable.card_65, R.drawable.card_66, R.drawable.card_67,
-            R.drawable.card_68, R.drawable.card_69, R.drawable.card_70,
-            R.drawable.card_71, R.drawable.card_72, R.drawable.card_73,
-            R.drawable.card_74, R.drawable.card_75, R.drawable.card_76,
-            R.drawable.card_77, R.drawable.card_78, R.drawable.card_79,
-            R.drawable.card_80, R.drawable.card_81, };
+            R.drawable.card_2,
+            R.drawable.card_3,
+            R.drawable.card_4,
+            R.drawable.card_5,
+            R.drawable.card_6,
+            R.drawable.card_7,
+            R.drawable.card_8,
+            R.drawable.card_9,
+            R.drawable.card_10,
+            R.drawable.card_11,
+            R.drawable.card_12,
+            R.drawable.card_13,
+            R.drawable.card_14,
+            R.drawable.card_15,
+            R.drawable.card_16,
+            R.drawable.card_17,
+            R.drawable.card_18,
+            R.drawable.card_19,
+            R.drawable.card_20,
+            R.drawable.card_21,
+            R.drawable.card_22,
+            R.drawable.card_23,
+            R.drawable.card_24,
+            R.drawable.card_25,
+            R.drawable.card_26,
+            R.drawable.card_27,
+            R.drawable.card_28,
+            R.drawable.card_29,
+            R.drawable.card_30,
+            R.drawable.card_31,
+            R.drawable.card_32,
+            R.drawable.card_33,
+            R.drawable.card_34,
+            R.drawable.card_35,
+            R.drawable.card_36,
+            R.drawable.card_37,
+            R.drawable.card_38,
+            R.drawable.card_39,
+            R.drawable.card_40,
+            R.drawable.card_41,
+            R.drawable.card_42,
+            R.drawable.card_43,
+            R.drawable.card_44,
+            R.drawable.card_45,
+            R.drawable.card_46,
+            R.drawable.card_47,
+            R.drawable.card_48,
+            R.drawable.card_49,
+            R.drawable.card_50,
+            R.drawable.card_51,
+            R.drawable.card_52,
+            R.drawable.card_53,
+            R.drawable.card_54,
+            R.drawable.card_55,
+            R.drawable.card_56,
+            R.drawable.card_57,
+            R.drawable.card_58,
+            R.drawable.card_59,
+            R.drawable.card_60,
+            R.drawable.card_61,
+            R.drawable.card_62,
+            R.drawable.card_63,
+            R.drawable.card_64,
+            R.drawable.card_65,
+            R.drawable.card_66,
+            R.drawable.card_67,
+            R.drawable.card_68,
+            R.drawable.card_69,
+            R.drawable.card_70,
+            R.drawable.card_71,
+            R.drawable.card_72,
+            R.drawable.card_73,
+            R.drawable.card_74,
+            R.drawable.card_75,
+            R.drawable.card_76,
+            R.drawable.card_77,
+            R.drawable.card_78,
+            R.drawable.card_79,
+            R.drawable.card_80,
+            R.drawable.card_81,       };
 }
